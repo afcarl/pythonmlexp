fd9c71de439787ec9cfa41c30bb2095dc952728e
muvic08 <muvic08@gmail.com>
2016-09-23 20:58:58 -0400

    brownian.py comment edit

--- brownian.py
+++ brownian.py
@@ -10,12 +10,12 @@ Brownian Motion:
     
     Brownian motion over [0,T] is a r.v. W(t) that depends continuously
     on t elem_of [0,T] and satisfies the following three conditions:
-    1. W(0) = 0 (with probability 1)
-    2. For 0<=s<t<=T ther r.v. given by the incremnt W(t)-W(s) is
-        normally distr' with mean 0 and variance t-s, equivalently,
+    1. W(0) = 0 (with probability 1).
+    2. For 0<=s<t<=T the r.v. given by the incremnt W(t)-W(s) is
+        normally distr' with mean 0 and variance t-s; equivalently,
         W(t)-W(s)~=sqrt(t-s)*N(0,1), where N(0,1) denotes norm' dist'
         r.v. with 0 mean and unit variance.
-    3. For 0<=s<t<u<v<=T the incrememts W(t)-W(s) and W(v)-W(u) are independent
+    3. For 0<=s<t<u<v<=T the incrememts W(t)-W(s) and W(v)-W(u) are independent.
     
     
     Algorithm:
